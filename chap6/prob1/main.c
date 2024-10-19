e58a01538a429dea72779df97937e856fc002e1a
dongyun <galshml1205@gmail.com>
2024-10-19 13:45:23 +0000

    git commit -m 'chap6_prob1'

---
+++ chap6/prob1/main.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct stat buf;
+	for (i = 1; i < argc; i++) {
+		printf("%s: ", argv[i]);
+		if (lstat(argv[i], &buf) < 0) {
+		    perror("lstat()");
+			continue;
+		}
+		if (S_ISREG(buf.st_mode))
+		    printf("%s \n", "general file");
+		if (S_ISDIR(buf.st_mode))
+			printf("%s \n", "directory");
+		if (S_ISCHR(buf.st_mode))
+			printf("%s \n", "text device file");
+	    if (S_ISBLK(buf.st_mode))
+			printf("%s \n", "block device file");
+		if (S_ISFIFO(buf.st_mode))
+			printf("%s \n", "FIFO file");
+		if (S_ISLNK(buf.st_mode))
+			printf("%s \n", "symblock link");
+	    if (S_ISSOCK(buf.st_mode))
+		    printf("%s \n", "socket");
+	  }
+	   exit(0);
+	}
+
+
+
+
+
+
+
